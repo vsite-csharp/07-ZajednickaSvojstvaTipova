4d2814094b1c8058ec0181bd5dad6fc50c9341d0
Auro Mazzi <auro.mazzi@gmail.com>
2019-12-08 20:13:50 +0100

    Resolve GetHashCode for strings

--- GetHashCodeStringa/GetHashCodeStringa.cs
+++ GetHashCodeStringa/GetHashCodeStringa.cs
@@ -7,7 +7,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
         static void Main(string[] args)
         {
             // TODO:080 Pokrenuti program i usporediti ispise. Koja dva znakovna niza daju najbliže brojeve?
-
+            // cat i cut
             Console.WriteLine(string.Format("cat.GetHashCode() = {0}", "cat".GetHashCode()));
             Console.WriteLine(string.Format("cta.GetHashCode() = {0}", "cta".GetHashCode()));
             Console.WriteLine(string.Format("cap.GetHashCode() = {0}", "cap".GetHashCode()));
