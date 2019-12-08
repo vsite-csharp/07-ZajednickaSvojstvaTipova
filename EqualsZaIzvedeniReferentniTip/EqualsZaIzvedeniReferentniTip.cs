147099f61b98f5b8f71f89043412f940e7bb961b
Auro Mazzi <auro.mazzi@gmail.com>
2019-12-08 20:03:49 +0100

    Resolve Equals for derived reference type

--- EqualsZaIzvedeniReferentniTip/EqualsZaIzvedeniReferentniTip.cs
+++ EqualsZaIzvedeniReferentniTip/EqualsZaIzvedeniReferentniTip.cs
@@ -4,22 +4,38 @@ using System.Diagnostics;
 namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 {
     // TODO:040 Definirati da klasa Student implementira sučelje IEquatable<Student>
-    class Student : Osoba
+    class Student : Osoba, IEquatable<Student>
     {
         public Student(string ime, int matičniBroj, string smjer, int godina) : base(ime, matičniBroj)
         {
             m_smjer = smjer;
             m_godina = godina;
+            m_ime = ime;
+            m_matičniBroj = matičniBroj;
         }
 
         string m_smjer;
         int m_godina;
+        string m_ime;
+        int m_matičniBroj;
 
         // TODO:041 Implementirati metodu Equals(Student) iz sučelja IEquatable<Student> da uključi dodatne usporedbe da bi studenti bili jednaki samo ako su na istom smjeru i godini.
-
+        public bool Equals(Student stud)
+        {
+            if (stud == null || !m_ime.Equals(stud.m_ime) || !m_matičniBroj.Equals(stud.m_matičniBroj))
+                return false;
+            if (m_smjer.Equals(stud.m_smjer) && m_godina.Equals(stud.m_godina))
+                return true;
+            return false;
+        }
 
         // TODO:042 Nadglasati (override) metodu Equals(object) tako da poziva metodu Equals(Student).
-
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            return Equals(obj as Student);
+        }
 
 
         public override string ToString()
