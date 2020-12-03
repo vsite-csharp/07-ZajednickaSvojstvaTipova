24ceb54f96bc46d9cf1a922aac9c11d262c203bd
Paolo Stabile <pastabil@vsite.hr>
2020-12-03 21:22:18 +0100

    Resolve GetHashCode

--- EqualsZaReferentniTip/EqualsZaReferentniTip.cs
+++ EqualsZaReferentniTip/EqualsZaReferentniTip.cs
@@ -32,7 +32,10 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
         {
             return Equals(obj as Osoba);
         }
-
+        public override int GetHashCode()
+        {
+            return ime.GetHashCode() ^ matičniBroj.GetHashCode();
+        }
         public override string ToString()
         {
             return $"'{ime}, {matičniBroj}'";
