df8da86568345901813a0dd54aa79b88f8ad1d4a
Filip Lončar <filoncar@vsite.hr>
2020-12-03 20:49:38 +0100

    Resolve  Equals for derived referneces

--- EqualsZaIzvedeniReferentniTip/EqualsZaIzvedeniReferentniTip.cs
+++ EqualsZaIzvedeniReferentniTip/EqualsZaIzvedeniReferentniTip.cs
@@ -3,8 +3,8 @@ using System.Diagnostics;
 
 namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 {
-    // TODO:041 Definirati da klasa Student implementira sučelje IEquatable<Student>
-    class Student : Osoba
+    // ++TODO:041 Definirati da klasa Student implementira sučelje IEquatable<Student>
+    class Student : Osoba, IEquatable<Student>
     {
         public Student(string ime, int matičniBroj, string smjer, int godina) : base(ime, matičniBroj)
         {
@@ -15,16 +15,27 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
         string smjer;
         int godina;
 
-        // TODO:042 Implementirati metodu Equals(Student) iz sučelja IEquatable<Student> da uključi dodatne usporedbe da bi studenti bili jednaki samo ako su na istom smjeru i godini.
-
+        // ++TODO:042 Implementirati metodu Equals(Student) iz sučelja IEquatable<Student> da uključi dodatne usporedbe da bi studenti bili jednaki samo ako su na istom smjeru i godini.
+        public bool Equals(Student other)
+        {
+            if (base.Equals(other) == false)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return smjer == other.smjer && godina == other.godina;
+            //throw new NotImplementedException();
+        }
 
         // TODO:043 Nadglasati (override) metodu Equals(object) tako da poziva metodu Equals(Student).
 
 
         public override string ToString()
         {
+            //return Equals(obj as Student);
             return $"{base.ToString()} ({smjer} {godina}.godina)";
         }
+
+        
     }
 
     class EqualsZaIzvedeniReferentiTip
@@ -55,7 +66,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 
         }
 
-        // TODO:040 Pokrenuti program i pogledati ispis.
+        // ++TODO:040 Pokrenuti program i pogledati ispis.
         static void Main(string[] args)
         {
             // dva različita studenta
