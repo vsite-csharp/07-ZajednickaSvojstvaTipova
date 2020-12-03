78279489c600686a04af414c10dc51241c988b1d
Ante Žunić <anzunic@vsite.hr>
2020-12-03 21:02:35 +0100

    Resolve Equality for refrence type

--- EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
+++ EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
@@ -6,7 +6,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
     public class EqualsZaVrijednosniTip
     {
         // TODO:051 Definirati da struktura Osoba implementira sučelje IEquatable<Osoba>
-        public struct Osoba
+        public struct Osoba : IEquatable<Osoba>
         {
             public Osoba(string ime, int matičniBroj)
             {
@@ -18,10 +18,16 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             int matičniBroj;
 
             // TODO:052 Implementirati metodu Equals(Osoba) iz sučelja IEquatable<Osoba> tako da za osobe s istim matičnim brojem rezultat bude true (bez obzira na ime)
-
+            public bool Equals(Osoba other)
+            {
+                throw new NotImplementedException();
+            }
 
             // TODO:053 Nadglasati (override) metodu Equals(object) tako da poziva Equals(Osoba)
-
+            public override bool Equals(object obj)
+            {
+                return Equals(obj);
+            }
 
             public override string ToString()
             {
@@ -32,6 +38,8 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             {
                 ime = novoIme;
             }
+
+            
         }
 
         public static void UsporedbaOsoba(Osoba osobaA, Osoba osobaB)
@@ -43,7 +51,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             Console.WriteLine(osobaB.Equals(osobaA));
         }
 
-        // TODO:050 Pokrenuti program i pogledati ispis.
+        // Pokrenuti program i pogledati ispis.
         static void Main(string[] args)
         {
             // dvije osobe s različitim imenima i MB
