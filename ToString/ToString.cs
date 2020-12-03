85bd90bc6940462ac9148c7f16a9bdfc717f7483
Franjo Gregorić <franjo.gregoric@triple-innovations.com>
2020-12-03 19:40:05 +0100

    ToString override

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -14,6 +14,11 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
         public double Imaginarni;
 
         // TODO:011 Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
+        public override string ToString()
+        {
+            // Treba stavit da je realni - imaginarni ako je tako
+            return $"{Realni}+{Imaginarni}j";
+        }
 
     }
 
