767ff1424d30d6c4d7f1a06c07cd23aef02db339
Zvonimir Dražić <zvonimir.drazic@gmail.com>
2020-12-03 20:00:01 +0100

    Resolve Equals

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -13,7 +13,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
         public double Realni;
         public double Imaginarni;
 
-        //  Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
+        //  TODO: 011 DZ  Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
         public override string ToString()
         {
             /*if(Realni>=0 && Imaginarni>=0)*/return $"{Realni}+{Imaginarni}j";
