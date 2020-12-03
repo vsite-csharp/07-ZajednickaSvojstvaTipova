b12a230da6f5fc8fd66fde059a4631605ae5235f
Ante Žunić <anzunic@vsite.hr>
2020-12-03 21:21:49 +0100

    Resolve get hash code

--- EqualsZaReferentniTip/EqualsZaReferentniTip.cs
+++ EqualsZaReferentniTip/EqualsZaReferentniTip.cs
@@ -30,6 +30,11 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             return Equals(obj as Osoba);
         }
 
+        public override int GetHashCode()
+        {
+            return ime.GetHashCode() ^ matičniBroj.GetHashCode();
+        }
+
         public override string ToString()
         {
             return $"'{ime}, {matičniBroj}'";
