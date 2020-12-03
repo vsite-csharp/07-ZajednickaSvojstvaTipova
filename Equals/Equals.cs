81fb33e504d6aff4407ded142e1acfc56927041f
Valentina Behin <72219767+ValentinaBehin@users.noreply.github.com>
2020-12-03 19:56:58 +0100

    Resolve equals

--- Equals/Equals.cs
+++ Equals/Equals.cs
@@ -17,33 +17,35 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             MojaKlasa mk1 = new MojaKlasa();
             MojaKlasa mk2 = null;
 
-            // TODO:020 Usporediti objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.Equals te ispisati rezultat
+            //  Usporediti objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.Equals te ispisati rezultat
+            Console.WriteLine(MojaKlasa.Equals(mk1, mk2));
 
 
-            // TODO:021 Usporediti reference na objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.ReferenceEquals te ispisati rezultat
+            //  Usporediti reference na objekte mk1 i mk2 korištenjem statičke metode MojaKlasa.ReferenceEquals te ispisati rezultat
+            Console.WriteLine(MojaKlasa.ReferenceEquals(mk1, mk2));
 
+            //Usporediti objekte mk1 i mk2 pozivom metode mk1.Equals te ispisati rezultat
+            Console.WriteLine(mk1.Equals(mk2));
 
-            // TODO:022 Usporediti objekte mk1 i mk2 pozivom metode mk1.Equals te ispisati rezultat
-
-
-            // TODO:023 Usporediti objekte mk1 i mk2 pozivom metode mk2.Equals te ispisati rezultat
-
+            //  Usporediti objekte mk1 i mk2 pozivom metode mk2.Equals te ispisati rezultat
+           // Console.WriteLine(mk2.Equals(mk1));
 
         }
 
         public static void UsporedbaStruktura()
         {
-            // TODO:024 Stvoriti dvije strukture ms1 i ms2 na isti način kao i za gornje instance klasa mk1 i mk2
+            //  Stvoriti dvije strukture ms1 i ms2 na isti način kao i za gornje instance klasa mk1 i mk2
+            MojaStruktura ms1 = new MojaStruktura();
+            MojaStruktura ms2 = new MojaStruktura();
 
+            //  Usporediti objekte ms1 i ms2 korištenjem statičke metode MojaStruktura.Equals te ispisati rezultat
+            Console.WriteLine(MojaStruktura.Equals(ms1, ms2));
 
-            // TODO:025 Usporediti objekte ms1 i ms2 korištenjem statičke metode MojaStruktura.Equals te ispisati rezultat
-
-
-            // TODO:026 Usporediti objekte ms1 i ms2 pozivom metode ms1.Equals te ispisati rezultat
-
+            // Usporediti objekte ms1 i ms2 pozivom metode ms1.Equals te ispisati rezultat
+            Console.WriteLine(ms1.Equals(ms2));
 
             // TODO:027 Usporediti objekte ms1 i ms2 pozivom metode ms2.Equals te ispisati rezultat
-
+            Console.WriteLine(ms2.Equals(ms1));
         }
 
         static void Main(string[] args)
