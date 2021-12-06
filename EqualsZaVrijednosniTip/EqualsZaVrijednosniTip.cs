091e303f09eb23ff1c25b265f1e86d08f84a3af2
Pavle Pavicic <ppavicic@dspace.hr>
2021-12-06 14:59:23 +0100

    Solved all TODO:5*.

--- EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
+++ EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
@@ -6,7 +6,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
     public class EqualsZaVrijednosniTip
     {
         // TODO:051 Definirati da struktura Osoba implementira sučelje IEquatable<Osoba>
-        public struct Osoba
+        public struct Osoba : IEquatable<Osoba>
         {
             public Osoba(string ime, int matičniBroj)
             {
@@ -19,9 +19,27 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 
             // TODO:052 Implementirati metodu Equals(Osoba) iz sučelja IEquatable<Osoba> tako da za osobe s istim matičnim brojem rezultat bude true (bez obzira na ime)
 
+            public bool Equals(Osoba other)
+            {
+                if (!Equals(this.matičniBroj, other.matičniBroj))
+                {
+                    return false;
+                }
+
+                return true;
+            }
 
             // TODO:053 Nadglasati (override) metodu Equals(object) tako da poziva Equals(Osoba)
 
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Osoba))
+                {
+                    return false;
+                }
+
+                return Equals((Osoba) obj);
+            }
 
             public override string ToString()
             {
@@ -43,7 +61,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             Console.WriteLine(osobaB.Equals(osobaA));
         }
 
-        // TODO:050 Pokrenuti program i pogledati ispis.
+        // Pokrenuti program i pogledati ispis.
         static void Main(string[] args)
         {
             // dvije osobe s različitim imenima i MB
