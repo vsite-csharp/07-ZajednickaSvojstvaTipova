3fcb1fd9ddf3a341354a67a851d5710c32efeffb
Antonio Brkic <ABrkic@dspace.hr>
2021-12-06 13:16:57 +0100

    Remove TODO's from complex ToString

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -51,9 +51,9 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
     // 010 Pokrenuti program i pogledati ispise instanci strukture KompleksniBroj.
     class ToString
     {
-        // TODO:012 Pokrenuti program i usporediti ispise s očekivanima.
+        // 012 Pokrenuti program i usporediti ispise s očekivanima.
 
-        // TODO:013 Pokrenuti testove u grupi TestToString i provjeriti prolaze li svi testovi.
+        // 013 Pokrenuti testove u grupi TestToString i provjeriti prolaze li svi testovi.
         static void Main(string[] args)
         {
             KompleksniBroj kb = new KompleksniBroj(2, 3);
