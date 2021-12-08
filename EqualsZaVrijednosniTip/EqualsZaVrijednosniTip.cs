744074a6210ec911c483ac689d0b7fb5866df6c7
Ana Lovric <alovric@dspace.hr>
2021-12-08 18:52:39 +0100

    DZ: Resloved Equals method for value type

--- EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
+++ EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
@@ -5,8 +5,8 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 {
     public class EqualsZaVrijednosniTip
     {
-        // TODO:051 Definirati da struktura Osoba implementira sučelje IEquatable<Osoba>
-        public struct Osoba
+        // 051 Definirati da struktura Osoba implementira sučelje IEquatable<Osoba>
+        public struct Osoba: IEquatable<Osoba>
         {
             public Osoba(string ime, int matičniBroj)
             {
@@ -17,11 +17,17 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             string ime;
             int matičniBroj;
 
-            // TODO:052 Implementirati metodu Equals(Osoba) iz sučelja IEquatable<Osoba> tako da za osobe s istim matičnim brojem rezultat bude true (bez obzira na ime)
+            // 052 Implementirati metodu Equals(Osoba) iz sučelja IEquatable<Osoba> tako da za osobe s istim matičnim brojem rezultat bude true (bez obzira na ime)
+            public bool Equals(Osoba other)
+            {
+               return ((this.matičniBroj == other.matičniBroj));
+            }
 
-
-            // TODO:053 Nadglasati (override) metodu Equals(object) tako da poziva Equals(Osoba)
-
+            // 053 Nadglasati (override) metodu Equals(object) tako da poziva Equals(Osoba)
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as Osoba?);
+            }
 
             public override string ToString()
             {
@@ -43,7 +49,7 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
             Console.WriteLine(osobaB.Equals(osobaA));
         }
 
-        // TODO:050 Pokrenuti program i pogledati ispis.
+        // 050 Pokrenuti program i pogledati ispis.
         static void Main(string[] args)
         {
             // dvije osobe s različitim imenima i MB
