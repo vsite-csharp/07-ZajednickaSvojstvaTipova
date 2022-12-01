11101363ffc9504efc641b944a9e808efa7bf6ff
Marko Bohač <mabohac@vsite.hr>
2022-12-01 21:30:37 +0100

    Resolved equals for reference types

--- EqualsZaReferentniTip/EqualsZaReferentniTip.cs
+++ EqualsZaReferentniTip/EqualsZaReferentniTip.cs
@@ -3,104 +3,118 @@ using System.Diagnostics;
 
 namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 {
-    // TODO:031 Definirati da klasa Osoba implementira sučelje IEquatable<Osoba>
-    public class Osoba
-    {
-        public Osoba(string ime, int matičniBroj)
+	// TODO:031 Definirati da klasa Osoba implementira sučelje IEquatable<Osoba>
+	public class Osoba : IEquatable<Osoba>
+	{
+		public Osoba(string ime, int matičniBroj)
+		{
+			this.ime = ime;
+			this.matičniBroj = matičniBroj;
+		}
+
+		string ime;       // član referentnog tipa
+		int matičniBroj;  // član vrijednosnog tipa
+
+		// TODO:032 Implementirati metodu Equals(Osoba) iz sučelja IEquatable<Osoba> tako da za osobe s istim imenom i istim matičnim brojem rezultat bude true
+
+
+		// TODO:033 Nadglasati (override) metodu Equals(object) tako da poziva Equals(Osoba)
+
+
+		public override string ToString()
+		{
+			return $"'{ime}, {matičniBroj}'";
+		}
+
+		public void PromijeniIme(string novoIme)
+		{
+			ime = novoIme;
+		}
+        public override bool Equals(object? obj)
         {
-            this.ime = ime;
-            this.matičniBroj = matičniBroj;
+            if (obj == null)
+                return false;
+			if (obj.GetType() != typeof(Osoba))
+				return false;
+			return Equals((Osoba)obj);
         }
+        public bool Equals(Osoba? other)
+		{
+			if (other == null)
+				return false;
+			return ime == other.ime && matičniBroj == other.matičniBroj;
+		}
+	}
 
-        string ime;       // član referentnog tipa
-        int matičniBroj;  // član vrijednosnog tipa
+	class EqualsZaReferentniTip
+	{
+		public static void UsporedbaOsoba(Osoba osobaA, Osoba osobaB)
+		{
+			Console.WriteLine(osobaA);
+			Console.WriteLine(osobaB);
 
-        // TODO:032 Implementirati metodu Equals(Osoba) iz sučelja IEquatable<Osoba> tako da za osobe s istim imenom i istim matičnim brojem rezultat bude true
+			try
+			{
+				Console.WriteLine(osobaA.Equals(osobaB));
+			}
+			catch (NullReferenceException)
+			{
+				Console.WriteLine("POGREŠKA: osobaA je null referenca pa nema metodu Equals!");
+			}
+			try
+			{
+				Console.WriteLine(osobaB.Equals(osobaA));
+			}
+			catch (NullReferenceException)
+			{
+				Console.WriteLine("POGREŠKA: osobaB je null referenca pa nema metodu Equals!");
+			}
 
+			// poziv statičke metode
+			Console.WriteLine(Osoba.Equals(osobaA, osobaB));
 
-        // TODO:033 Nadglasati (override) metodu Equals(object) tako da poziva Equals(Osoba)
+			Console.WriteLine(Osoba.ReferenceEquals(osobaA, osobaB));
+		}
 
-
-        public override string ToString()
-        {
-            return $"'{ime}, {matičniBroj}'";
-        }
-
-        public void PromijeniIme(string novoIme)
-        {
-            ime = novoIme;
-        }
-    }
-
-    class EqualsZaReferentniTip
-    {
-        public static void UsporedbaOsoba(Osoba osobaA, Osoba osobaB)
-        {
-            Console.WriteLine(osobaA);
-            Console.WriteLine(osobaB);
-
-            try
-            {
-                Console.WriteLine(osobaA.Equals(osobaB));
-            }
-            catch (NullReferenceException)
-            {
-                Console.WriteLine("POGREŠKA: osobaA je null referenca pa nema metodu Equals!");
-            }
-            try
-            {
-                Console.WriteLine(osobaB.Equals(osobaA));
-            }
-            catch (NullReferenceException)
-            {
-                Console.WriteLine("POGREŠKA: osobaB je null referenca pa nema metodu Equals!");
-            }
-
-            // poziv statičke metode
-            Console.WriteLine(Osoba.Equals(osobaA, osobaB));
-
-            Console.WriteLine(Osoba.ReferenceEquals(osobaA, osobaB));
-        }
-
-        // TODO:030 Pokrenuti program bez debuggera (Ctrl+F5) i pogledati ispis
-        static void Main(string[] args)
-        {
-            Util.IspisNaslova("Usporedba referenci na isti objekt");
-            Osoba osobaA = new Osoba("Janko", 1);
-            Osoba osobaB = osobaA;
-            UsporedbaOsoba(osobaA, osobaB);
+		// 030 Pokrenuti program bez debuggera (Ctrl+F5) i pogledati ispis
+		static void Main(string[] args)
+		{
+			Util.IspisNaslova("Usporedba referenci na isti objekt");
+			Osoba osobaA = new Osoba("Janko", 1);
+			Osoba osobaB = osobaA;
+			UsporedbaOsoba(osobaA, osobaB);
   
-            Console.WriteLine();
+			Console.WriteLine();
 
-            Util.IspisNaslova("Usporedba s null referencom na objekt istog tipa");
-            UsporedbaOsoba(osobaA, null);
-            
-            Console.WriteLine();
+			Util.IspisNaslova("Usporedba s null referencom na objekt istog tipa");
+			UsporedbaOsoba(osobaA, null);
+			
+			Console.WriteLine();
 
-            Util.IspisNaslova("Usporedba dviju osoba s različitim imenima i matičnim brojevima");
-            osobaB = new Osoba("Marko", 2);
-            UsporedbaOsoba(osobaA, osobaB);
-            
-            Console.WriteLine();
+			Util.IspisNaslova("Usporedba dviju osoba s različitim imenima i matičnim brojevima");
+			osobaB = new Osoba("Marko", 2);
+			UsporedbaOsoba(osobaA, osobaB);
+			
+			Console.WriteLine();
 
-            Util.IspisNaslova("Usporedba dviju osoba s istim imenima i različitim matičnim brojevima");
-            osobaB = new Osoba("Janko", 5);
-            UsporedbaOsoba(osobaA, osobaB);
-            
-            Console.WriteLine();
+			Util.IspisNaslova("Usporedba dviju osoba s istim imenima i različitim matičnim brojevima");
+			osobaB = new Osoba("Janko", 5);
+			UsporedbaOsoba(osobaA, osobaB);
+			
+			Console.WriteLine();
 
-            Util.IspisNaslova("Usporedba dviju osoba s istim imenima i istim matičnim brojevima");
-            osobaB = new Osoba("Janko", 1);
-            UsporedbaOsoba(osobaA, osobaB);
-            
-            Console.WriteLine();
+			Util.IspisNaslova("Usporedba dviju osoba s istim imenima i istim matičnim brojevima");
+			osobaB = new Osoba("Janko", 1);
+			UsporedbaOsoba(osobaA, osobaB);
+			
+			Console.WriteLine();
 
-            Util.IspisNaslova("Usporedba bezimene osobe s osobom koja ima ime");
-            osobaB = new Osoba(null, 2);
-            UsporedbaOsoba(osobaA, osobaB);
-            
-            Console.WriteLine("\nGOTOVO!!!");
-            Console.ReadKey();
-        }
-    }
+			Util.IspisNaslova("Usporedba bezimene osobe s osobom koja ima ime");
+			osobaB = new Osoba(null, 2);
+			UsporedbaOsoba(osobaA, osobaB);
+			
+			Console.WriteLine("\nGOTOVO!!!");
+			Console.ReadKey();
+		}
+	}
 }
