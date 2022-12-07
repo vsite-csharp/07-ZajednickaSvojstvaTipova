07a5bb96f0a157747d944d5d2d8d72ecb66d2e9d
Fran Štambuk <fran.stambuk@gmail.com>
2022-12-07 23:01:21 +0100

    Homework done. Implemented nicer ToString method output.

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -16,7 +16,45 @@ namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
         // 011 Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
         public override string ToString()
         {
-            return $"{Realni}+{Imaginarni}j";
+            if (Imaginarni == 0)
+            {
+                return $"{Realni}";
+            }
+            else if (Realni == 0)
+            {
+                if (Imaginarni == 1)
+                {
+                    return $"j";
+                }
+                else if (Imaginarni == -1)
+                {
+                    return $"-j";
+                }
+                else
+                {
+                    return $"{Imaginarni}j";
+                }
+            }
+            else
+            {
+                if (Imaginarni == 1)
+                {
+                    return $"{Realni}+j";
+                }
+                else if (Imaginarni == -1)
+                {
+                    return $"{Realni}-j";
+                }
+                else if (Imaginarni < 0)
+                {
+                    return $"{Realni}{Imaginarni}j";
+                }
+                else
+                {
+                    return $"{Realni}+{Imaginarni}j";
+                }
+
+            }
         }
 
     }
