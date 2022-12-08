6abbdd8b316306dcb7ac8015731176d4d4d2dd42
Marko Bohač <mabohac@vsite.hr>
2022-12-08 17:52:04 +0100

    Resolved ToString

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -2,65 +2,67 @@
 
 namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 {
-    struct KompleksniBroj
-    {
-        public KompleksniBroj(double realni, double imaginarni)
-        {
-            Realni = realni;
-            Imaginarni = imaginarni;
-        }
+	struct KompleksniBroj
+	{
+		public KompleksniBroj(double realni, double imaginarni)
+		{
+			Realni = realni;
+			Imaginarni = imaginarni;
+		}
 
-        public double Realni;
-        public double Imaginarni;
+		public double Realni;
+		public double Imaginarni;
 
-        // TODO:011 Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
-        public override string ToString()
-        {
-            return $"{Realni}+{Imaginarni}j";
-        }
+		// 011 Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
+		public override string ToString()
+		{
+			string realni = Realni == 0 ? "" : Imaginarni > 0 ? $"{Realni}+" : $"{Realni}";
+			string imaginarni = Imaginarni == 0 ? "" : Imaginarni == -1 ? "-j" : Imaginarni == 1 ? "j" : $"{Imaginarni}j";
+			return realni == "" && imaginarni == "" ? "0" : realni + imaginarni;
+		}
 
-    }
+	}
 
-    // 010 Pokrenuti program i pogledati ispise instanci strukture KompleksniBroj.
-    class ToString
-    {
-        // 012 Pokrenuti program i usporediti ispise s očekivanima.
+	// 010 Pokrenuti program i pogledati ispise instanci strukture KompleksniBroj.
+	class ToString
+	{
+		// 012 Pokrenuti program i usporediti ispise s očekivanima.
 
-        // TODO:013 Pokrenuti testove u grupi TestToString i provjeriti prolaze li svi testovi.
-        static void Main(string[] args)
-        {
-            KompleksniBroj kb = new KompleksniBroj(2, 3);
-            Console.WriteLine(kb); // "2+3j"
+		// 013 Pokrenuti testove u grupi TestToString i provjeriti prolaze li svi testovi.
+		static void Main(string[] args)
+		{
+			KompleksniBroj kb = new KompleksniBroj(2, 3);
+			Console.WriteLine(kb); // "2+3j"
 
-            kb.Imaginarni = -3;
-            Console.WriteLine(kb); // "2-3j"
+			kb.Imaginarni = -3;
+			Console.WriteLine(kb); // "2-3j"
 
             kb.Realni = 0;
-            Console.WriteLine(kb); // "-3j"
+			Console.WriteLine(kb); // "-3j"
 
-            kb.Imaginarni = 0;
-            Console.WriteLine(kb); // "0"
+			kb.Imaginarni = 0;
+			Console.WriteLine(kb); // "0"
 
-            kb.Imaginarni = 1;
-            Console.WriteLine(kb); // "j"
+			kb.Imaginarni = 1;
+			Console.WriteLine(kb); // "j"
 
-            kb.Imaginarni = -1;
-            Console.WriteLine(kb); // "-j"
+			kb.Imaginarni = -1;
+			Console.WriteLine(kb); // "-j"
 
-            kb.Imaginarni = 0;
-            kb.Realni = -2;
-            Console.WriteLine(kb); // "-2"
+			kb.Imaginarni = 0;
+			kb.Realni = -2;
+			Console.WriteLine(kb); // "-2"
 
-            kb.Imaginarni = -1;
-            kb.Realni = -2;
-            Console.WriteLine(kb); // "-2-j"
+			kb.Imaginarni = -1;
+			kb.Realni = -2;
+			Console.WriteLine(kb); // "-2-j"
 
-            kb.Imaginarni = 1;
-            kb.Realni = 2;
-            Console.WriteLine(kb); // "2+j"
+			kb.Imaginarni = 1;
+			kb.Realni = 2;
+			Console.WriteLine(kb); // "2+j"
 
-            Console.WriteLine("\nGOTOVO!!!");
-            Console.ReadKey();
-        }
-    }
+			Console.WriteLine("\nGOTOVO!!!");
+			Console.ReadKey();
+		}
+	}
 }
