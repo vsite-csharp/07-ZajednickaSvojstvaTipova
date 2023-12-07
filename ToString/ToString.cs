5f660360b4f32a66b4b15ea752a59773ff665137
Bogjenero <bornagjenero1@gmail.com>
2023-12-07 20:40:56 +0100

    Start implementation of ToString override

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -11,15 +11,24 @@
         public double Realni;
         public double Imaginarni;
 
-        // TODO:011 Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
-        
-
+        // :011 Nadglasati (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
+        public override string ToString()
+        {
+            if(Imaginarni > 0)
+                return $"{Realni}+{Imaginarni}j";
+            else
+                return $"{Realni}-{Imaginarni}j";
+        }
+        private string ImagnaryPart()
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    // TODO:010 Pokrenuti program i pogledati ispise instanci strukture KompleksniBroj.
+    // :010 Pokrenuti program i pogledati ispise instanci strukture KompleksniBroj.
     static class ToString
     {
-        // TODO:012 Pokrenuti program i usporediti ispise s očekivanima.
+        // :012 Pokrenuti program i usporediti ispise s očekivanima.
 
         // TODO:013 Pokrenuti testove u grupi TestToString i provjeriti prolaze li svi testovi.
         static void Main()
