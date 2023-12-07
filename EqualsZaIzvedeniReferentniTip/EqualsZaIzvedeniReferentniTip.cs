fc5fe047b87fb3bfb9f902bc3fc6bb6c0a8487f7
Kristian Mesić <kristian.mesic303@gmail.com>
2023-12-07 21:19:37 +0100

    - check Equals for Reference types; Need to check

--- EqualsZaIzvedeniReferentniTip/EqualsZaIzvedeniReferentniTip.cs
+++ EqualsZaIzvedeniReferentniTip/EqualsZaIzvedeniReferentniTip.cs
@@ -1,7 +1,7 @@
 ﻿namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 {
     // TODO:041 Definirati da klasa Student implementira sučelje IEquatable<Student>
-    class Student : Osoba
+    class Student : Osoba, IEquatable<Student>
     {
         public Student(string ime, int matičniBroj, string smjer, int godina) : base(ime, matičniBroj)
         {
@@ -13,15 +13,26 @@
         private readonly int godina;
 
         // TODO:042 Implementirati metodu Equals(Student) iz sučelja IEquatable<Student> da uključi dodatne usporedbe da bi studenti bili jednaki samo ako su na istom smjeru i godini.
+        public bool Equals(Student? other)
+        {
+            return other != null &&
+                base.Equals(other) &&
+                smjer == other.smjer &&
+                godina == other.godina;
+        }
 
-
-        // TODO:043 Nadglasati (override) metodu Equals(object) tako da poziva metodu Equals(Student).
-
+        // :043 Nadglasati (override) metodu Equals(object) tako da poziva metodu Equals(Student).
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj as Student);
+        }
 
         public override string ToString()
         {
             return $"{base.ToString()} ({smjer} {godina}.godina)";
         }
+
+
     }
 
     static class EqualsZaIzvedeniReferentiTip
@@ -52,31 +63,31 @@
 
         }
 
-        // TODO:040 Pokrenuti program i pogledati ispis.
+        // :040 Pokrenuti program i pogledati ispis.
         static void Main()
         {
             // dva različita studenta
             Student studentA = new Student("Janko", 1, "Programiranje", 3);
             Student studentB = new Student("Darko", 2, "Administriranje", 2);
 
-            UsporedbaStudenata(studentA, studentB);
+            UsporedbaStudenata(studentA, studentB); //false
 
             Console.WriteLine();
 
             // novi "Janko" s istim matičnim brojem, isti smjer i godina
             studentB = new Student("Janko", 1, "Programiranje", 3);
-            UsporedbaStudenata(studentA, studentB);
+            UsporedbaStudenata(studentA, studentB); //false
 
             Console.WriteLine();
 
             // "Janko", ali na drugoj godini
             studentB = new Student("Janko", 1, "Programiranje", 2);
-            UsporedbaStudenata(studentA, studentB);
+            UsporedbaStudenata(studentA, studentB); //false
 
             Console.WriteLine();
 
             studentA = studentB;
-            UsporedbaStudenata(studentA, studentB);
+            UsporedbaStudenata(studentA, studentB); // true
 
             Console.WriteLine("\nGOTOVO!!!");
         }
