989e0580efd4d306bb4a17055dbfd03927d7e001
Bogjenero <bornagjenero1@gmail.com>
2023-12-14 19:30:15 +0100

    Fix equals for value type and implement equality operator

--- EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
+++ EqualsZaVrijednosniTip/EqualsZaVrijednosniTip.cs
@@ -1,4 +1,6 @@
-﻿namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
+﻿using System.Data.Common;
+
+namespace Vsite.CSharp.ZajedničkaSvojstvaTipova
 {
     public class EqualsZaVrijednosniTip
     {
@@ -32,17 +34,25 @@
 
             public override bool Equals(object? obj)
             {
-                        
-                if(obj == null) return false;
+
+                if (obj == null) return false;
 
                 return Equals((Osoba)(obj));
             }
 
             public bool Equals(Osoba osoba)
             {
-                return ime == osoba.ime &&
-                      matičniBroj == osoba.matičniBroj;
+                return matičniBroj == osoba.matičniBroj;
             }
+            public static bool operator ==(Osoba left, Osoba right)
+            {
+                return left.Equals(right);
+            }
+            public static bool operator!=(Osoba left, Osoba right)
+            {
+                return!(left==right);
+            }
+
         }
 
         public static void UsporedbaOsoba(Osoba osobaA, Osoba osobaB)
--- JednakostZaVrijednosniTip/JedankostZaVrijednosniTip.cs
+++ JednakostZaVrijednosniTip/JedankostZaVrijednosniTip.cs
@@ -6,11 +6,11 @@
     {
         public static void Jednakost(Osoba osobaA, Osoba osobaB)
         {
-            // TODO:070 Otkomentirati donje naredbe i provjeriti može li se kod prevesti i izvesti.
-            //Console.WriteLine($"{osobaA} == {osobaB}: {osobaA == osobaB}");
-            //Console.WriteLine($"{osobaA} != {osobaB}: {osobaA != osobaB}");
+            // :070 Otkomentirati donje naredbe i provjeriti može li se kod prevesti i izvesti.
+            Console.WriteLine($"{osobaA} == {osobaB}: {osobaA == osobaB}");
+            Console.WriteLine($"{osobaA} != {osobaB}: {osobaA != osobaB}");
 
-            // TODO:071 Strukturi Osoba u projektu EqualsZaVrijednosniTip definirati operatore == i != tako da donji primjeri daju očekivane rezultate.
+            // :071 Strukturi Osoba u projektu EqualsZaVrijednosniTip definirati operatore == i != tako da donji primjeri daju očekivane rezultate.
         }
 
         static void Main()
